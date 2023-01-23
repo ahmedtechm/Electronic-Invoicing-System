36f6b9ef882e35716da11db342c4f310657c3e26
ahmedtechm <ahmedtechm2050@gmail.com>
2023-01-23 11:36:06 +0400

    create a scanner to read input from the user

--- src/InvoicingSystem/Invoice.java
+++ src/InvoicingSystem/Invoice.java
@@ -1,6 +1,7 @@
 package InvoicingSystem;
 
 import java.io.Serializable;
+import java.util.Scanner;
 
 public class Invoice implements Serializable {
 	
@@ -12,6 +13,27 @@ public class Invoice implements Serializable {
 	
 
 	public static void main(String[] args) {
+		
+		Invoice invoice = new Invoice();
+        
+		
+		Scanner scan = new Scanner(System.in);
+        
+		System.out.println("Enter Invoice Date");
+        invoice.invoiceDate = scan.nextInt();
+        
+        System.out.println("Enter Number Of Item");
+        invoice.numberOfItem = scan.nextInt();
+        
+        System.out.println("Enter Total Amoun");
+        invoice.totalAmount = scan.nextDouble();
+        
+        System.out.println("Enter Paid Amount");
+        invoice.paidAmount = scan.nextDouble();
+        
+        System.out.println("Enter Balance");
+        invoice.balance = scan.nextDouble();
+        
 		
 
 	}
--- src/InvoicingSystem/Item.java
+++ src/InvoicingSystem/Item.java
@@ -1,6 +1,7 @@
 package InvoicingSystem;
 
 import java.io.Serializable;
+import java.util.Scanner;
 
 public class Item implements Serializable {
 	
@@ -8,9 +9,30 @@ public class Item implements Serializable {
 	String itemName;
 	double itemPrice;
 	int itemQuantity;
+	double qtyAmountPrice;
 	
 
 	public static void main(String[] args) {
+		
+		Item item = new Item();
+        
+		
+		Scanner scan = new Scanner(System.in);
+        
+		System.out.println("Enter item ID Number");
+		item.itemID = scan.nextInt();
+        
+        System.out.println("Enter item Name ");
+        item.itemName = scan.nextLine();
+        
+        System.out.println("Enter item Price");
+        item.itemPrice = scan.nextDouble();
+        
+        System.out.println("Enter item Quantity");
+        item.itemQuantity = scan.nextInt();
+        
+        System.out.println("Enter qty Amount Price");
+        item.qtyAmountPrice = scan.nextDouble();
 		
 
 	}
