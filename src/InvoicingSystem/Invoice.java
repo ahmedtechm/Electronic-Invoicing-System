06c6cafe3a930eee9c0e6d6d56a4c7ed03bd3ba2
ahmedtechm <ahmedtechm2050@gmail.com>
2023-01-23 10:28:34 +0400

    adding Solo Project to github

---
+++ src/InvoicingSystem/Invoice.java
@@ -0,0 +1,10 @@
+package InvoicingSystem;
+
+public class Invoice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/InvoicingSystem/Item.java
@@ -0,0 +1,10 @@
+package InvoicingSystem;
+
+public class Item {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
